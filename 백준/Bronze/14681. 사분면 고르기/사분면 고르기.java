3b458d50dec8c6980191b038e7d44197f7e6153b
김지원 <106079745+ppaio12@users.noreply.github.com>
2022-07-04 11:36:10 +0900

    [Bronze V] Title: 사분면 고르기, Time: 204 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A;
+		int B;
+		
+		A = sc.nextInt();
+		B = sc.nextInt();
+		
+		if(A > 0 && B > 0) System.out.println(1);
+		else if(A < 0 && B > 0) System.out.println(2);
+		else if(A < 0 && B < 0) System.out.println(3);
+		else System.out.println(4);
+		
+	}	
+}
