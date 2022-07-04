6c8688ba131ebaf8959543237d9b8ea93b6d82fb
김지원 <106079745+ppaio12@users.noreply.github.com>
2022-07-04 11:41:09 +0900

    [Bronze III] Title: 기찍 N, Time: 1116 ms, Memory: 32536 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int N;
+		
+		N = sc.nextInt();
+		
+		for(int i = N; i > 0; i--) {
+			System.out.println(i);
+		}
+		
+	}
+}
